f6914060f5053c99da3f85bcb97438ba1ad4e72b
Guy Mininberg <Guy.Mininberg@vonage.com>
2017-01-14 16:39:20 +0200

    proxy server

---
+++ small_proxy_server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://www.google.com' });
+}).listen(3000);
